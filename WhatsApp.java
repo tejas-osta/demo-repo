4f6da1f849e3365a1e76336c09efc02cf0ea06ad
tejas-osta <tejas@ostaapp.com>
2021-06-23 17:13:54 +0530

    remove extras

--- WhatsApp.java
+++ WhatsApp.java
@@ -7,17 +7,11 @@ class WhatsApp{
 
 	// feature 1
 	public static void message(){
-		System.out.println("Feature 1");
 		System.out.println("Message");	
 	}
 
 	// feature 2
-<<<<<<< HEAD
-	public static void calling(){
-=======
 	public static void calling()
->>>>>>> 8b83d04cea918b46319340c7c681f9e944e83073
-		System.out.println("Feature 2");
 		System.out.println("Calling");	
 	}
 }
