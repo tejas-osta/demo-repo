8b83d04cea918b46319340c7c681f9e944e83073
tejas-osta <tejas@ostaapp.com>
2021-06-23 12:18:50 +0530

    calling feature

--- WhatsApp.java
+++ WhatsApp.java
@@ -2,11 +2,18 @@ class WhatsApp{
 
 	public static void main(String[] args){
 		message();
+		calling();
 	}
 
 	// feature 1
 	public static void message(){
 		System.out.println("Feature 1");
 		System.out.println("Message");	
+	}
+
+	// feature 2
+	public static void calling()
+		System.out.println("Feature 2");
+		System.out.println("Calling");	
 	}
 }
