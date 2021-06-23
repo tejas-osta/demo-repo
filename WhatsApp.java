04ed8482131d4ab5a62224e058244bbe3978a052
tejas-osta <tejas@ostaapp.com>
2021-06-23 12:08:56 +0530

    msg feature

---
+++ WhatsApp.java
@@ -0,0 +1,12 @@
+class WhatsApp{
+
+	public static void main(String[] args){
+		message();
+	}
+
+	// feature 1
+	public static void message(){
+		System.out.println("Feature 1");
+		System.out.println("Message");	
+	}
+}
